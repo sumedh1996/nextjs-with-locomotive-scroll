73d95f55f14bff8b825a38a9c430ec4cf8791211
sumedh raj <sumedh@tealfeed.com>
2022-08-11 19:11:40 +0530

    Using Locomotive Scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
+import { LocomotiveScrollProvider as ReactLocomotiveScrollProvider } from 'react-locomotive-scroll';
 
+import 'locomotive-scroll/dist/locomotive-scroll.css';
+import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { asPath } = useRouter();
+  const containerRef = useRef(null);
+  return (
+    <ReactLocomotiveScrollProvider
+      options={{
+        smooth: true,
+      }}
+      watch={
+        []
+      }
+      location={asPath}
+      onLocationChange={(scroll) => scroll.scrollTo(0, { duration: 0, disableLerp: true })}
+      containerRef={containerRef}
+    >
+      <div data-scroll-container ref={containerRef}>
+        <Component {...pageProps} />;
+      </div>
+    </ReactLocomotiveScrollProvider>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useLocomotiveScroll } from "react-locomotive-scroll";
 
 export default function Home() {
+  // const { scroll } = useLocomotiveScroll();
   return (
     <div className={styles.container}>
       <Head>
@@ -12,43 +14,121 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+        <div data-scroll-section>
+          <h1
+            className={styles.title}
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="6"
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+          <p
+            className={styles.description}
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="-5"
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            Get started by editing{" "}
+            <code className={styles.code}>pages/index.tsx</code>
+          </p>
+
+          <div className={styles.grid}>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <h2>Documentation &rarr;</h2>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h2>Learn &rarr;</h2>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
+
+            <a
+              href="https://github.com/vercel/next.js/tree/master/examples"
+              className={styles.card}
+            >
+              <h2>Examples &rarr;</h2>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
+
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+            >
+              <h2>Deploy &rarr;</h2>
+              <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </div>
+        </div>
+
+        <div data-scroll-section>
+          <p className={styles.description} data-scroll data-scroll-speed="1">
+            Simple Scroll Speed
+          </p>
+
+          <div className={styles.grid}>
+            {[...Array(20).keys()].map((idx) => (
+              <div
+                className={styles.card}
+                key={idx}
+                data-scroll
+                data-scroll-speed={idx}
+              >
+                <h2>data-scroll-speed</h2>
+                <code>{`<div data-scroll data-scroll-speed="${idx}"></div>`}</code>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div data-scroll-section>
+          <p className={styles.description} data-scroll data-scroll-speed="1">
+            Scroll Delay
+          </p>
+
+          <div className={styles.grid}>
+            {[...Array(10).keys()].map((idx) => (
+              <div
+                className={styles.card}
+                key={idx}
+                data-scroll
+                data-scroll-speed="1"
+                data-scroll-delay={idx / 100}
+              >
+                <h2>data-scroll-delay</h2>
+                <code>{`<div data-scroll data-scroll-speed="1" data-scroll-delay="${
+                  idx / 100
+                }"></div>`}</code>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div data-scroll-section>
+          <p className={styles.description} data-scroll data-scroll-speed="1">
+            Scroll Speed + Delay
+          </p>
+
+          <div className={styles.grid}>
+            {[...Array(10).keys()].map((idx) => (
+              <div
+                className={styles.card}
+                key={idx}
+                data-scroll
+                data-scroll-speed={idx}
+                data-scroll-delay={idx / 100}
+              >
+                <h2>data-scroll-delay</h2>
+                <code>{`<div data-scroll data-scroll-speed="${idx}" data-scroll-delay="${
+                  idx / 100
+                }"></div>`}</code>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
 
@@ -58,12 +138,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
